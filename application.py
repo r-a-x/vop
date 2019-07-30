b4b027337bf6e3507bca558d33770b03bf5e8c59
Rahul Bansal <bansal.rahul14@gmail.com>
2019-07-30 23:31:53 +0530

    added the initial api

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import json
 
-from user import signup_user, insert_contacts, search_mutual, has_user_signup
+from video import get_video_feed
 
 app = Flask(__name__)
 
@@ -14,61 +14,14 @@ def throw_error(code, success, message):
     }), 500
 
 
-def return_json(mp):
-    return json.dumps(mp)
-
-
-@app.route("/api/signup", methods=['POST'])
-def signup():
-    request_json = request.json
-    if "name" not in request_json or request_json["name"] is None:
-        return throw_error(-1, False, "Name is None")
-    else:
-        name = request_json["name"]
-    if "number" not in request_json or request_json["number"] is None:
-        return throw_error(-1, False, "Phone Number is None")
-    else:
-        number = request_json["number"]
-    return return_json(signup_user(name=name, number=number))
-
-
-@app.route("/api/contacts", methods=['POST', 'GET'])
-def contacts():
-    request_json = request.json
-    if "user" not in request_json or request_json["user"] is None:
-        return throw_error(-1, False, "User is not present")
-    if "contacts" not in request_json or request_json["contacts"] is None:
-        return throw_error(-1, False, "Contacts are not present")
-    user = request_json["user"]
-    name, number = user["name"], user["number"]
-    if number is None:
-        return throw_error(-1, False, "Number is not present")
-    contactslist = request_json["contacts"]
-    if contactslist is None:
-        return throw_error(-1, False, "Contact list is empty")
-    return json.dumps(insert_contacts(user, contactslist))
-
-
-@app.route("/api/search", methods=['GET'])
-def search_mock():
-    source_number = request.args.get('source')
-    destination_number = request.args.get('destination')
-    if source_number is None or destination_number is None:
-        return throw_error(-1, False, "Source or Destination number not mentioned")
-    return json.dumps(search_mutual(source_number, destination_number))
-
-
 @app.route("/")
 def hello():
     return "Hello There!!"
 
 
-@app.route("/api/signup", methods=['GET'])
-def user_signup():
-    number = request.args.get('number')
-    if number is None or len(number) != 10:
-        throw_error(-1, False, "Number can not be empty")
-    return json.dumps(has_user_signup(number))
+@app.route("/feed")
+def feed():
+    return json.dumps(get_video_feed())
 
 
 if __name__ == "__main__":
---
+++ video.py
@@ -0,0 +1,259 @@
+
+def get_video_feed():
+    return {
+        "videoFeed": [
+            {
+                "ReviewId": "701",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=Ei6CpdcXKpE"
+            },
+            {
+                "ReviewId": "712",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=BdqYsE38xzs"
+            },
+            {
+
+                "ReviewId": "713",
+                "ReviewTitle": "Bangkok Travel Guide",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=a3W0igEWrHI"
+            },
+            {
+                "ReviewId": "714",
+                "ReviewTitle": "Ladakh",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=9JfKGrcjapM"
+            },
+            {
+                "ReviewId": "721",
+                "ReviewTitle": "Maldives Trip",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": " https://www.youtube.com/watch?v=c4yfRxSVzeE"
+            },
+            {
+                "ReviewId": "701",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=Ei6CpdcXKpE"
+            },
+            {
+                "ReviewId": "712",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=BdqYsE38xzs"
+            },
+            {
+
+                "ReviewId": "713",
+                "ReviewTitle": "Bangkok Travel Guide",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=a3W0igEWrHI"
+            },
+            {
+                "ReviewId": "714",
+                "ReviewTitle": "Ladakh",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=9JfKGrcjapM"
+            },
+            {
+                "ReviewId": "721",
+                "ReviewTitle": "Maldives Trip",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": " https://www.youtube.com/watch?v=c4yfRxSVzeE"
+            },
+            {
+                "ReviewId": "701",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=Ei6CpdcXKpE"
+            },
+            {
+                "ReviewId": "712",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=BdqYsE38xzs"
+            },
+            {
+                "ReviewId": "713",
+                "ReviewTitle": "Bangkok Travel Guide",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=a3W0igEWrHI"
+            },
+            {
+                "ReviewId": "714",
+                "ReviewTitle": "Ladakh",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=9JfKGrcjapM"
+            },
+            {
+                "ReviewId": "721",
+                "ReviewTitle": "Maldives Trip",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": " https://www.youtube.com/watch?v=c4yfRxSVzeE"
+            },
+            {
+                "ReviewId": "701",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=Ei6CpdcXKpE"
+            },
+            {
+                "ReviewId": "712",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=BdqYsE38xzs"
+            },
+            {
+
+                "ReviewId": "713",
+                "ReviewTitle": "Bangkok Travel Guide",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=a3W0igEWrHI"
+            },
+            {
+                "ReviewId": "714",
+                "ReviewTitle": "Ladakh",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=9JfKGrcjapM"
+            },
+            {
+                "ReviewId": "721",
+                "ReviewTitle": "Maldives Trip",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": " https://www.youtube.com/watch?v=c4yfRxSVzeE"
+            },
+            {
+                "ReviewId": "701",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=Ei6CpdcXKpE"
+            },
+            {
+                "ReviewId": "712",
+                "ReviewTitle": "Jodhpur Review",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=BdqYsE38xzs"
+            },
+            {
+                "ReviewId": "713",
+                "ReviewTitle": "Bangkok Travel Guide",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=a3W0igEWrHI"
+            },
+            {
+                "ReviewId": "714",
+                "ReviewTitle": "Ladakh",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": "https://www.youtube.com/watch?v=9JfKGrcjapM"
+            },
+            {
+                "ReviewId": "721",
+                "ReviewTitle": "Maldives Trip",
+                "RedirectLink": "https://www.makemytrip.com",
+                "ReviewCategory": "Travel",
+                "publishedAt": 12122012,
+                "relevance": 87.1,
+                "likes": 121,
+                "ReviewVideoUrl": " https://www.youtube.com/watch?v=c4yfRxSVzeE"
+            }
+        ]
+    }
